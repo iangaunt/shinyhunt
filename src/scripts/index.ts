2fbcbab635e701bd1ecd745cf9b724ec5fde7542
Ian G <66448861+iangaunt@users.noreply.github.com>
2023-09-18 15:25:35 -0700

    Initial commit

---
+++ src/scripts/index.ts
@@ -0,0 +1,61 @@
+let encounters: number = 0;
+
+// Displaying images for the chosen Pokemon.
+const poke_name = <HTMLInputElement> document.getElementById("poke-name");
+
+poke_name.addEventListener("input", function() {
+    let img: HTMLImageElement = <HTMLImageElement> document.getElementById("poke-img");
+    img.src = "https://pokemon.night.coffee/icons/shiny/" + poke_name.value.toLowerCase() + ".gif";
+});
+
+// Updating counters.
+const counter = document.getElementById("counter");
+const add = <HTMLButtonElement> document.getElementById("add");
+const subtract = <HTMLButtonElement> document.getElementById("subtract");
+
+add.addEventListener("click", function() {
+    encounters++;
+    counter.innerHTML = encounters.toString();
+    generatePercentages();
+})
+
+subtract.addEventListener("click", function() {
+    if (encounters - 1 >= 0) {
+        encounters--;
+        counter.innerHTML = encounters.toString();
+        generatePercentages();
+    }
+})
+
+// Updating odds.
+const odds = <HTMLInputElement> document.getElementById("odd");
+odds.addEventListener("input", function() {
+    generatePercentages();
+});
+
+function getBnP(n: number, p: number) {
+    return 100 * (1 - Math.pow((p - 1) / p, n))
+}
+function element(id: string): HTMLElement { 
+    return document.getElementById(id); 
+}
+
+function generatePercentages() {
+    element("current-odd").innerHTML = (Math.round(100 * getBnP(encounters, parseInt(odds.value))) / 100).toString() + "%";
+    for (let i = 10; i < 100; i += 10) {
+        let s: string = i.toString();
+        let count = 0;
+
+        while (getBnP(count, parseInt(odds.value)) <= i) {
+            count++;
+        }
+        element(s + "-until").innerHTML = count - encounters + " until";
+        element(s).innerHTML = count.toString();
+    }
+}
+
+function setEncounters(p: number) {
+    encounters = p;
+    counter.innerHTML = encounters.toString();
+    generatePercentages();
+}
---
+++ src/scripts/js/scripts/index.js
@@ -0,0 +1,51 @@
+var encounters = 0;
+// Displaying images for the chosen Pokemon.
+var poke_name = document.getElementById("poke-name");
+poke_name.addEventListener("input", function () {
+    var img = document.getElementById("poke-img");
+    img.src = "https://pokemon.night.coffee/icons/shiny/" + poke_name.value.toLowerCase() + ".gif";
+});
+// Updating counters.
+var counter = document.getElementById("counter");
+var add = document.getElementById("add");
+var subtract = document.getElementById("subtract");
+add.addEventListener("click", function () {
+    encounters++;
+    counter.innerHTML = encounters.toString();
+    generatePercentages();
+});
+subtract.addEventListener("click", function () {
+    if (encounters - 1 >= 0) {
+        encounters--;
+        counter.innerHTML = encounters.toString();
+        generatePercentages();
+    }
+});
+// Updating odds.
+var odds = document.getElementById("odd");
+odds.addEventListener("input", function () {
+    generatePercentages();
+});
+function getBnP(n, p) {
+    return 100 * (1 - Math.pow((p - 1) / p, n));
+}
+function element(id) {
+    return document.getElementById(id);
+}
+function generatePercentages() {
+    element("current-odd").innerHTML = (Math.round(100 * getBnP(encounters, parseInt(odds.value))) / 100).toString() + "%";
+    for (var i = 10; i < 100; i += 10) {
+        var s = i.toString();
+        var count = 0;
+        while (getBnP(count, parseInt(odds.value)) <= i) {
+            count++;
+        }
+        element(s + "-until").innerHTML = count - encounters + " until";
+        element(s).innerHTML = count.toString();
+    }
+}
+function setEncounters(p) {
+    encounters = p;
+    counter.innerHTML = encounters.toString();
+    generatePercentages();
+}
